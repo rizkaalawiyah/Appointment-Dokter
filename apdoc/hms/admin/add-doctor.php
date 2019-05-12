6e1d6feaf3aa7e0e5f55ae629d849b905c5229fb
rizkaalawiyah <rizkaalawiyah88@gmail.com>
2019-05-12 15:57:23 +0700

    si

---
+++ apdoc/hms/admin/add-doctor.php
@@ -0,0 +1,223 @@
+<?php
+session_start();
+include('include/config.php');
+include('include/checklogin.php');
+check_login();
+
+if(isset($_POST['submit']))
+{	$docspecialization=$_POST['Doctorspecialization'];
+$docname=$_POST['docname'];
+$docaddress=$_POST['clinicaddress'];
+$docfees=$_POST['docfees'];
+$doccontactno=$_POST['doccontact'];
+$docemail=$_POST['docemail'];
+$password=md5($_POST['npass']);
+$sql=mysqli_query($con,"insert into doctors(specilization,doctorName,address,docFees,contactno,docEmail,password) values('$docspecialization','$docname','$docaddress','$docfees','$doccontactno','$docemail','$password')");
+if($sql)
+{
+echo "<script>alert('Info Dokter berhasil ditambahkan!');</script>";
+header('location:manage-doctors.php');
+
+}
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Admin | Tambah  Dokter</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		<meta content="" name="description" />
+		<meta content="" name="author" />
+		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="vendor/themify-icons/themify-icons.min.css">
+		<link href="vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/select2/select2.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/bootstrap-datepicker/bootstrap-datepicker3.standalone.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/bootstrap-timepicker/bootstrap-timepicker.min.css" rel="stylesheet" media="screen">
+		<link rel="stylesheet" href="assets/css/styles.css">
+		<link rel="stylesheet" href="assets/css/plugins.css">
+		<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
+<script type="text/javascript">
+function valid()
+{
+ if(document.adddoc.npass.value!= document.adddoc.cfpass.value)
+{
+alert("Password and Confirm Password Field do not match  !!");
+document.adddoc.cfpass.focus();
+return false;
+}
+return true;
+}
+</script>
+
+	</head>
+	<body>
+		<div id="app">		
+<?php include('include/sidebar.php');?>
+			<div class="app-content">
+				
+						<?php include('include/header.php');?>
+						
+
+				<div class="main-content" >
+					<div class="wrap-content container" id="container">
+
+						<section id="page-title">
+							<div class="row">
+								<div class="col-sm-8">
+									<h1 class="mainTitle">Admin |  Tambah Dokter</h1>
+																	</div>
+								<ol class="breadcrumb">
+									<li>
+										<span>Admin</span>
+									</li>
+									<li class="active">
+										<span> Tambah Dokter</span>
+									</li>
+								</ol>
+							</div>
+						</section>
+						<div class="container-fluid container-fullw bg-white">
+							<div class="row">
+								<div class="col-md-12">
+									
+									<div class="row margin-top-30">
+										<div class="col-lg-8 col-md-12">
+											<div class="panel panel-white">
+												<div class="panel-heading">
+													<h5 class="panel-title">Tambah Dokter</h5>
+												</div>
+												<div class="panel-body">
+									
+													<form role="form" name="adddoc" method="post" onSubmit="return valid();">
+														<div class="form-group">
+															<label for="DoctorSpecialization">
+																Spesialisasi Dokter
+															</label>
+							<select name="Doctorspecialization" class="form-control" required="required">
+																<option value="">Buka Spesialisasi</option>
+<?php $ret=mysqli_query($con,"select * from doctorspecilization");
+while($row=mysqli_fetch_array($ret))
+{
+?>
+																<option value="<?php echo htmlentities($row['specilization']);?>">
+																	<?php echo htmlentities($row['specilization']);?>
+																</option>
+																<?php } ?>
+																
+															</select>
+														</div>
+
+<div class="form-group">
+															<label for="doctorname">
+																 Nama Dokter
+															</label>
+					<input type="text" name="docname" class="form-control"  placeholder="Masukan Nama Dokter">
+														</div>
+
+
+<div class="form-group">
+															<label for="address">
+																 Alamat Klinik Dokter
+															</label>
+					<textarea name="clinicaddress" class="form-control"  placeholder="Masukan Alamat Klinik Dokter"></textarea>
+														</div>
+<div class="form-group">
+															<label for="fess">
+																 Biaya Konsultasi
+															</label>
+					<input type="text" name="docfees" class="form-control"  placeholder="Masukan Biaya Konsultasi Dokter">
+														</div>
+	
+<div class="form-group">
+									<label for="fess">
+																 Kontak
+															</label>
+					<input type="text" name="doccontact" class="form-control"  placeholder="Masukan Kontak Dokter">
+														</div>
+
+<div class="form-group">
+									<label for="fess">
+																E-Mail
+															</label>
+					<input type="email" name="docemail" class="form-control"  placeholder="Masukan E-Mail Dokter">
+														</div>
+
+
+
+														
+														<div class="form-group">
+															<label for="exampleInputPassword1">
+																 Password
+															</label>
+					<input type="password" name="npass" class="form-control"  placeholder="Password Baru" required="required">
+														</div>
+														
+<div class="form-group">
+															<label for="exampleInputPassword2">
+																Konfirmasi Password
+															</label>
+									<input type="password" name="cfpass" class="form-control"  placeholder="Konfirmasi Password" required="required">
+														</div>
+														
+														
+														
+														<button type="submit" name="submit" class="btn btn-o btn-primary">
+															Submit
+														</button>
+													</form>
+												</div>
+											</div>
+										</div>
+											
+											</div>
+										</div>
+									<div class="col-lg-12 col-md-12">
+											<div class="panel panel-white">
+												
+												
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+	<?php include('include/footer.php');?>
+		
+		</div>
+		<script src="vendor/jquery/jquery.min.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+		<script src="vendor/modernizr/modernizr.js"></script>
+		<script src="vendor/jquery-cookie/jquery.cookie.js"></script>
+		<script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+		<script src="vendor/switchery/switchery.min.js"></script>
+		<script src="vendor/maskedinput/jquery.maskedinput.min.js"></script>
+		<script src="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
+		<script src="vendor/autosize/autosize.min.js"></script>
+		<script src="vendor/selectFx/classie.js"></script>
+		<script src="vendor/selectFx/selectFx.js"></script>
+		<script src="vendor/select2/select2.min.js"></script>
+		<script src="vendor/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+		<script src="vendor/bootstrap-timepicker/bootstrap-timepicker.min.js"></script>
+		<script src="assets/js/main.js"></script>
+		<script src="assets/js/form-elements.js"></script>
+		<script>
+			jQuery(document).ready(function() {
+				Main.init();
+				FormElements.init();
+			});
+		</script>
+	</body>
+</html>
---
+++ apdoc/hms/admin/include/sidebar.php
@@ -0,0 +1,94 @@
+<div class="sidebar app-aside" id="sidebar">
+				<div class="sidebar-container perfect-scrollbar">
+
+<nav>
+						
+						<div class="navbar-title">
+							<span>Main Menu</span>
+						</div>
+						<ul class="main-navigation-menu">
+							<li>
+								<a href="dashboard.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-home"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Dashboard </span>
+										</div>
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Dokter </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									<li>
+										<a href="doctor-specilization.php">
+											<span class="title"> Spesialisasi Dokter </span>
+										</a>
+									</li>
+									<li>
+										<a href="add-doctor.php">
+											<span class="title"> Tambah Dokter</span>
+										</a>
+									</li>
+									<li>
+										<a href="Manage-doctors.php">
+											<span class="title"> Kelola Dokter </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+
+				<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Pasien </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="manage-users.php">
+											<span class="title"> Kelola Pasien </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>	
+
+<li>
+								<a href="appointment-history.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-file"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Appointment History </span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+
+
+						</ul>
+						
+					</nav>
+					</div>
+			</div>
